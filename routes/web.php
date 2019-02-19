660575146c55de2f8b6e75a9cc851a04d9043d02
chromosems <opudaemmanuel@gmail.com>
2019-02-19 13:20:18 +0300

    adding cars

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('cars', CarController::class);
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
